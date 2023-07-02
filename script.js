7a79147573b1dcb490794ffacdcac03ab56a761c
hugames6 <132520067+hugames6@users.noreply.github.com>
2023-07-02 17:47:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let tg = window.Telegram.WebApp;
+let mario = document.getElementById('mario');
+let einstein = document.getElementById('einstein');
+let info = ' ';
+tg.expand();
+
+mario.addEventListener('click', function(){
+    info = 'mario';
+});
+einstein.addEventListener('click', function(){
+    info = 'einstein';
+})
+tg.sendData(info);
+tg.close();
